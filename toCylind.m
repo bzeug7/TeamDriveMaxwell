9b935a6f1b6a7a4b74775233e6353d8643204717
bzeug7 <bzeug7@gmail.com>
2017-05-01 16:03:17 -0400

    changed x to z

--- toCylind.m
+++ toCylind.m
@@ -2,7 +2,7 @@ function [R theta Z] = toCylind(x, y, z)
    %Converts cartesian coordinates that give the position of each B vector
    %into polar coordinates. 
 
-theta=atan(z./y);
+theta=atan(x./y);
     %X is left alone
-    R= sqrt(x.^2+z.^2);
-    Z = x;
+    R= sqrt(x.^2+y.^2);
+    Z = z;
